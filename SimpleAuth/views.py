8b8e035caa9431d24cf4afbfca89fa2939b7a4d1
Dušan Andrić <dusan.andric@oicr.on.ca>
2017-07-05 17:03:27 -0400

    more stuff

--- SimpleAuth/views.py
+++ SimpleAuth/views.py
@@ -1,6 +1,8 @@
 from oauth2_provider.models import AccessToken
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist
+import json
 
 
 class TokenForm(forms.Form):
@@ -8,11 +10,32 @@ class TokenForm(forms.Form):
 
 
 def check_token(request):
+    '''
+    Let's match the spring security endpoint
+    :param request: 
+    :return: 
+    '''
+
     if request.method == 'POST':
         form = TokenForm(request.POST)
         if form.is_valid():
             token = form.cleaned_data['token']
-            accessToken = AccessToken.objects.get(token=token)
-            return HttpResponse(accessToken.scope)
 
-    return HttpResponseBadRequest('Go away.')
+            try:
+                access_token = AccessToken.objects.get(token=token)
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound(content=json.dumps({'error': 'NOT FOUND!', 'message': 'Go away.'}),
+                                            content_type='application/json')
+
+            test_token_response = {
+                "exp": 9999999999,
+                "user_name": "test@test.test",
+                "client_id": "clientId",
+                "scope": access_token.scope.split(",")
+            }
+            print(json.dumps(test_token_response))
+            return HttpResponse(content=json.dumps(test_token_response),
+                                content_type='application/json')
+
+    return HttpResponseBadRequest(content=json.dumps({'error': 'ERROR!', 'message': 'Go away.'}),
+                                  content_type='application/json')
